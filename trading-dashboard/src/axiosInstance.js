59ed0e783dde5c3ebfe2d18710afcce0e94208f8
Raphael Deem <raphael.deem@gmail.com>
2024-06-10 16:58:36 -0700

    more helm fixes

--- trading-dashboard/src/axiosInstance.js
+++ trading-dashboard/src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.API_URL || 'http://localhost:8000';
+
 const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8000',
+    baseURL: baseURL,
 });
 
 export default axiosInstance;
