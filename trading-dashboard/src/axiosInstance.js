063a90c7403b45e4a682a5daf2e57117129538cf
Raphael Deem <raphael.deem@gmail.com>
2024-06-10 19:19:29 -0700

    use REACT_ env vars

--- trading-dashboard/src/axiosInstance.js
+++ trading-dashboard/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.API_URL || 'http://localhost:8000';
+const baseURL = process.env.REACT_API_URL || 'http://localhost:8000';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
