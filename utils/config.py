bbde5c7e6b17c265dff5020a87491c514c17a254
Raphael Deem <raphael.deem@gmail.com>
2024-06-25 07:05:27 -0700

    parse rebalance interval

--- utils/config.py
+++ utils/config.py
@@ -47,9 +47,10 @@ def load_custom_strategy(broker, config):
         file_path = config['file_path']
         class_name = config['class_name']
         starting_capital = config['starting_capital']
+        rebalance_interval_minutes = config['rebalance_interval_minutes']
         strategy_class = load_strategy_class(file_path, class_name)
         logger.info(f"Initializing custom strategy '{class_name}' with config: {config}")
-        return strategy_class(broker, starting_capital, **config['strategy_params'])
+        return strategy_class(broker, starting_capital, rebalance_interval_minutes, **config['strategy_params'])
     except Exception as e:
         logger.error(f"Error initializing custom strategy '{config['class_name']}': {e}")
         raise
