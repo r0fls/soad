c1a519ddce1bd591f3fd24f913868b599dc7f551
Raphael Deem <raphael.deem@gmail.com>
2024-07-17 10:48:49 -0700

    add buffer to constant percentage strategy

--- strategies/constant_percentage_strategy.py
+++ strategies/constant_percentage_strategy.py
@@ -6,11 +6,12 @@ from strategies.base_strategy import BaseStrategy
 import asyncio
 
 class ConstantPercentageStrategy(BaseStrategy):
-    def __init__(self, broker, strategy_name, stock_allocations, cash_percentage, rebalance_interval_minutes, starting_capital):
+    def __init__(self, broker, strategy_name, stock_allocations, cash_percentage, rebalance_interval_minutes, starting_capital, buffer=0.1):
         self.stock_allocations = stock_allocations
         self.cash_percentage = cash_percentage
         self.rebalance_interval_minutes = rebalance_interval_minutes
         self.rebalance_interval = timedelta(minutes=rebalance_interval_minutes)
+        self.buffer = buffer
         super().__init__(broker, strategy_name, starting_capital)
         logger.info(
             f"Initialized {self.strategy_name} strategy with starting capital {self.starting_capital}")
@@ -49,9 +50,11 @@ class ConstantPercentageStrategy(BaseStrategy):
             current_position = current_positions.get(stock, 0)
             current_price = await self.broker.get_current_price(stock) if asyncio.iscoroutinefunction(self.broker.get_current_price) else self.broker.get_current_price(stock)
             target_quantity = target_balance // current_price
-            if current_position < target_quantity:
+            # If we own less than the target quantity plus or minus the buffer, buy more
+            if current_position < target_quantity * (1 - self.buffer):
                 await self.place_order(stock, target_quantity - current_position, 'buy', current_price)
-            elif current_position > target_quantity:
+            # If we own more than the target quantity plus or minus the buffer, sell the excess
+            elif current_position > target_quantity * (1 + self.buffer):
                 await self.place_order(stock, current_position - target_quantity, 'sell', current_price)
 
         for stock, quantity in current_db_positions_dict.items():
