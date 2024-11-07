5861d163975c0df784828668c6bc8926f60908fe
Raphael Deem <raphael.deem@gmail.com>
2024-11-06 20:03:10 -0800

    remove etrade

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ import os
 import importlib.util
 from brokers.tradier_broker import TradierBroker
 from brokers.tastytrade_broker import TastytradeBroker
-from brokers.etrade_broker import EtradeBroker
+from brokers.alpaca_broker import AlpacaBroker
 from sqlalchemy.ext.asyncio import create_async_engine
 from strategies.constant_percentage_strategy import ConstantPercentageStrategy
 from strategies.random_yolo_hedge_strategy import RandomYoloHedge
@@ -15,8 +15,8 @@ from .logger import logger
 # TODO: refactor
 BROKER_MAP = {
     'tradier': lambda config, engine: TradierBroker(api_key=config['api_key'], secret_key=None, engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
-    'etrade': lambda config, engine: EtradeBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
     'tastytrade': lambda config, engine: TastytradeBroker(username=config['username'], password=config['password'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False))
+    'alpaca': lambda config, engine: AlpacaBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False))
 }
 
 # Mapping of strategy types to their constructors
