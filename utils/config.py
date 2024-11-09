94f83762f463ef5d84604642cb0aa68699d4fb2e
Raphael Deem <raphael.deem@gmail.com>
2024-11-08 20:22:17 -0800

    helm fixes

--- utils/config.py
+++ utils/config.py
@@ -16,11 +16,31 @@ from .logger import logger
 # Mapping of broker types to their constructors
 # TODO: refactor
 BROKER_MAP = {
-    'tradier': lambda config, engine: TradierBroker(api_key=config['api_key'], secret_key=None, engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
-    'tastytrade': lambda config, engine: TastytradeBroker(username=config['username'], password=config['password'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
-    'alpaca': lambda config, engine: AlpacaBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False)),
-    'kraken': lambda config, engine: KrakenBroker(api_key=config['api_key'], secret_key=config['secret_key'], engine=engine, prevent_day_trading=config.get('prevent_day_trading', False))
+    'tradier': lambda config, engine: TradierBroker(
+        api_key=os.environ.get('TRADIER_API_KEY', config.get('api_key')),
+        secret_key=None,
+        engine=engine,
+        prevent_day_trading=config.get('prevent_day_trading', False)
+    ),
+    'tastytrade': lambda config, engine: TastytradeBroker(
+        username=os.environ.get('TASTYTRADE_USERNAME', config.get('username')),
+        password=os.environ.get('TASTYTRADE_PASSWORD', config.get('password')),
+        engine=engine,
+        prevent_day_trading=config.get('prevent_day_trading', False)
+    ),
+    'alpaca': lambda config, engine: AlpacaBroker(
+        api_key=os.environ.get('ALPACA_API_KEY', config.get('api_key')),
+        secret_key=os.environ.get('ALPACA_SECRET_KEY', config.get('secret_key')),
+        engine=engine,
+        prevent_day_trading=config.get('prevent_day_trading', False)
+    ),
+    'kraken': lambda config, engine: KrakenBroker(
+        api_key=os.environ.get('KRAKEN_API_KEY', config.get('api_key')),
+        secret_key=os.environ.get('KRAKEN_SECRET_KEY', config.get('secret_key')),
+        engine=engine
+    )
 }
+
 
 # Mapping of strategy types to their constructors
 STRATEGY_MAP = {
