5f1aa96b88cb890781c3fa2ce54fc1ef4a132bae
Raphael Deem <raphael.deem@gmail.com>
2024-11-10 21:15:41 -0800

    rename kraken secret

--- brokers/base_broker.py
+++ brokers/base_broker.py
@@ -14,13 +14,9 @@ from utils.utils import is_option, OPTION_MULTIPLIER, is_futures_symbol, futures
 class BaseBroker(ABC):
     def __init__(
             self,
-            api_key,
-            secret_key,
             broker_name,
             engine,
             prevent_day_trading=False):
-        self.api_key = api_key
-        self.secret_key = secret_key
         self.broker_name = broker_name.lower()
         self.db_manager = DBManager(engine)
         # Use AsyncSession
