9948723e1afbea2fa56327f70821ea75b4b64688
Raphael Deem <raphael.deem@gmail.com>
2024-11-24 23:10:51 -0800

    fix init

--- order_manager/manager.py
+++ order_manager/manager.py
@@ -2,8 +2,9 @@ from database.db_manager import DBManager
 from utils.logger import logger
 
 class OrderManager:
-    async def __init__(self, engine, brokers):
+    def __init__(self, engine, brokers):
         logger.info('Initializing OrderManager')
+        self.engine = engine
         self.db_manager = DBManager(engine)
         self.brokers = brokers
 
@@ -11,6 +12,7 @@ class OrderManager:
         logger.info('Reconciling orders', extra={'orders': orders})
         for order in orders:
             await self.reconcile_order(order)
+        # Commit the transaction
 
     async def reconcile_order(self, order):
         logger.info(f'Reconciling order {order.id}', extra={'order_id': order.id, 'broker': order.broker, 'symbol': order.symbol, 'quantity': order.quantity, 'price': order.price, 'side': order.side, 'status': order.status, 'type': order.type, 'time_in_force': order.time_in_force, 'created_at': order.created_at, 'updated_at': order.updated_at, 'filled_at': order.filled_at, 'filled_quantity': order.filled_quantity, 'remaining_quantity': order.remaining_quantity, 'canceled_at': order.canceled_at, 'failed_at': order.failed_at, 'message': order.message})
